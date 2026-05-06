5ebe74672d9fe1ddd8159f3e6d5027f079152e6f
Erol1978 <30065105@cc.peralta.edu>
2026-05-06 01:42:03 +0000

    Fix project structure for CMake build

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/cis25-final-project
+# Build directory: /workspaces/cis25-final-project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Tests "/workspaces/cis25-final-project/build/tests")
+set_tests_properties(Tests PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/cis25-final-project/CMakeLists.txt;13;add_test;/workspaces/cis25-final-project/CMakeLists.txt;0;")
